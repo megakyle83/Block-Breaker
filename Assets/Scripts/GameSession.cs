e02a7f18a2a9eb3e82b59694bb2ea47627258db1
megakyle83 <megakyle83@hotmail.com>
2021-01-27 12:21:13 -0800

    initial commit

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    //config params
+    [Range(0.0f, 10.0f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoplayEnabled;
+
+    //state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameSessionCount = FindObjectsOfType<GameSession>().Length;
+        if (gameSessionCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore = currentScore + pointsPerBlockDestroyed;
+        //simplified: currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+    public bool IsAutoplayEnabled()
+    {
+        return isAutoplayEnabled;
+    }
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+
+    {
+            SceneManager.LoadScene("Game Over");
+        }
+    
+
+}
